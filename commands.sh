03a4940a7b054ab4c41604d8dd9c9d3665168d56
mayounus <129637851+mayounus@users.noreply.github.com>
2023-06-04 10:21:29 -0400

    Updating files

---
+++ commands.sh
@@ -0,0 +1,11 @@
+#Source your python virtual environment (python3 -m venv ~/.myrepo)
+source ~/.myrepo/bin/activate
+
+#Project scaffolding
+make install
+
+# Run prediction
+./make_predict_azure_app.sh
+
+#Use Locust testing tool 
+locust -f locustfile.py --headless -t 3s
